f15e72d44e4e48ce41835d8f7305e23505e49920
HaVNguyen <vanhaxl@gmail.com>
2018-05-27 22:27:20 -0500

    finish 5.5 putting it all into practice

--- src/main/java/lambdasinaction/chap5/PuttingIntoPractice.java
+++ src/main/java/lambdasinaction/chap5/PuttingIntoPractice.java
@@ -77,15 +77,62 @@ public class PuttingIntoPractice {
 				.filter(trader -> trader.getCity() == "Cambridge").distinct().sorted(comparing(Trader::getName))
 				.collect(toList());
 		System.out.println(traders);
-		
+
 		// Query 4: Return a string of all traders’ names sorted alphabetically.
+		List<String> nameList = new ArrayList<>();
+		for (Transaction transaction : transactions) {
+			String name = transaction.getTrader().getName();
+			if (!nameList.contains(name)) {
+				nameList.add(name);
+			}
+		}
+		Collections.sort(nameList, (s1, s2) -> new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				return s1.compareTo(s2);
+			}
+		}.compare(s1, s2));
+		String tradersName = "";
+		for (String s : nameList) {
+			tradersName += s;
+		}
+		System.out.println(tradersName);
+
+		// use Java 8
+		String tradersName2 = transactions.stream().map(transaction -> transaction.getTrader().getName()).distinct()
+				.sorted().reduce("", (s1, s2) -> s1 + s2);
+		System.out.println(tradersName2);
 
 		// Query 5: Are there any trader based in Milan?
+		// normal
+		boolean milanBased = false;
+		for (Transaction transaction : transactions) {
+			if (transaction.getTrader().getCity() == "Milan") {
+				milanBased = true;
+				break;
+			}
+		}
+		System.out.println(milanBased);
+
+		// use Java 8
+		boolean milanBased2 = transactions.stream()
+				.anyMatch(transaction -> transaction.getTrader().getCity().equals("Milan"));
+		System.out.println(milanBased2);
 
 		// Query 6: Update all transactions so that the traders from Milan are set to
 		// Cambridge.
 
 		// Query 7: What's the highest value in all the transactions?
-
+		// normal
+		int max = Integer.MIN_VALUE;
+		for (Transaction transaction : transactions) {
+			if (transaction.getValue() > max)
+				max = transaction.getValue();
+		}
+		System.out.println(max);
+		
+		// use Java 8
+		Optional<Integer> max2 = transactions.stream().map(transaction -> transaction.getValue()).reduce(Integer::max);
+		max2.ifPresent(System.out::println);
 	}
 }
